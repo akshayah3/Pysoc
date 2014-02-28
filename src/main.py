d0365136a1bb7834f718c8252950e072b802bf2e
akshayah3 <akshaynukala95@gmail.com>
2014-02-28 18:01:11 +0530

    first commit

--- src/main.py
+++ src/main.py
@@ -1,2 +1,23 @@
 __author__ = 'Akshay'
 #todo Add the organisation class, student class, mentor class.
+
+from numpy import loadtxt
+class Gsoc:
+    """
+    Main class.
+    """
+    years = [2013, 2012, 2011, 2010, 2009]
+    def __init__(self):
+        return
+
+    def load_array(self, year):
+        if year in years:
+            return loadtxt('gsoc_' + str(year) + 'txt', dtype = 'string')
+
+    def get_organisation(self, year):
+            a = self.load_array(year)
+            return a
+
+    def get_student(self, organisation, year):
+        a = self.load_array(year)
+        return [i[1] for i in a if i[3] == organisation]
